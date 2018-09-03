ff512ec8dbe86d5d52ae3d1b47c98c43269036d7
Antonio Vanegas <hpsaturn@gmail.com>
2018-09-03 18:51:16 -0500

    refactor extra delay in some calls

--- src/main.cpp
+++ src/main.cpp
@@ -8,13 +8,16 @@ void blinkLoop(){
   digitalWrite(blinkPin, HIGH);
   delay(100);
   digitalWrite(blinkPin, LOW);
-  delay(100);
 }
 
 void clockLoop(){
+  char output [15];
   DateTime now = RTC.now();
-  char output [20];
-  sprintf(output,"echo %04d%02d%02d.%02d%02d%02d",now.year(),now.month(),now.day(),now.hour(),now.minute(),now.second());
+  sprintf(output,"echo %04d%02d%02d ",now.year(),now.month(),now.day());
+  DigiKeyboard.print(output);
+  sprintf(output,"%02d%02d",now.hour(),now.minute());
+  DigiKeyboard.print(output);
+  sprintf(output,"%02d",RTC.now().second());
   DigiKeyboard.println(output);
   delay(1000);
 }
@@ -23,8 +26,7 @@ void setup() {
   RTC.begin(DateTime(__DATE__, __TIME__));
   pinMode(blinkPin, OUTPUT);
   DigiKeyboard.sendKeyStroke(0);
-  DigiKeyboard.delay(100);
-  digitalWrite(blinkPin, HIGH);
+  DigiKeyboard.delay(1000);
 }
 
 void loop(){
