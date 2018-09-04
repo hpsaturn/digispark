42f5d641be0066fa30e7f1100006d1bf0a889201
Antonio Vanegas <hpsaturn@gmail.com>
2018-09-03 19:39:22 -0500

    not work soft rtc, only works in the next compiling execution

--- src/main.cpp
+++ src/main.cpp
@@ -11,14 +11,10 @@ void blinkLoop(){
 }
 
 void clockLoop(){
-  char output [15];
+  char output [7];
   DateTime now = RTC.now();
-  sprintf(output,"echo %04d%02d%02d ",now.year(),now.month(),now.day());
+  sprintf(output,"%02d%02d%02d ",now.hour(),now.minute(),now.second());
   DigiKeyboard.print(output);
-  sprintf(output,"%02d%02d",now.hour(),now.minute());
-  DigiKeyboard.print(output);
-  sprintf(output,"%02d",RTC.now().second());
-  DigiKeyboard.println(output);
   delay(1000);
 }
 
@@ -26,7 +22,7 @@ void setup() {
   RTC.begin(DateTime(__DATE__, __TIME__));
   pinMode(blinkPin, OUTPUT);
   DigiKeyboard.sendKeyStroke(0);
-  DigiKeyboard.delay(1000);
+  DigiKeyboard.delay(100);
 }
 
 void loop(){
