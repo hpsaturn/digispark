a03aa1a7029854f330ab634004f726ec93a6686f
Antonio Vanegas <hpsaturn@gmail.com>
2020-01-05 13:54:25 +0100

    removed DigiKeyboard base library implementation (internal bugs)

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,18 @@
-#include "DigiKeyboard.h"
-#include "TinyRTClib.h"
+#include <Arduino.h>
 
-RTC_Millis RTC;
 int blinkPin = 1;
 
 void blinkLoop(){
   digitalWrite(blinkPin, HIGH);
   delay(100);
   digitalWrite(blinkPin, LOW);
-}
-
-void clockLoop(){
-  char output [7];
-  DateTime now = RTC.now();
-  sprintf(output,"%02d%02d%02d ",now.hour(),now.minute(),now.second());
-  DigiKeyboard.print(output);
-  delay(1000);
+  delay(100);
 }
 
 void setup() {
-  RTC.begin(DateTime(__DATE__, __TIME__));
   pinMode(blinkPin, OUTPUT);
-  DigiKeyboard.sendKeyStroke(0);
-  DigiKeyboard.delay(100);
 }
 
 void loop(){
-  clockLoop();
   blinkLoop();
 }
