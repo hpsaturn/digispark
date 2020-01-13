5e2b391382ae268f6cbf61c74521b05d82b72e5a
Antonio Vanegas <hpsaturn@gmail.com>
2020-01-13 23:52:39 +0100

    fixed random seed initialization

--- src/main.cpp
+++ src/main.cpp
@@ -22,30 +22,29 @@
 #include <avr/interrupt.h>
 #include <avr/eeprom.h>
 
-#define BODS 7                   //BOD Sleep bit in MCUCR
-#define BODSE 2                  //BOD Sleep enable bit in MCUCR
-uint8_t mcucr1, mcucr2;
-
-#define RNDPIN      4      // for random generator
-
+Adafruit_NeoPixel *strip;
+int pixelFormat = NEO_GRB + NEO_KHZ800;
 #define FOCUS       20
 #define DELAY       1000
 #define WRAP        1     // wrap color wave
-Adafruit_NeoPixel *strip;
-int pin = 0;                             // Ring Led input
-int numPixels = 12; 
-int brightness = 30;
-int pixelFormat = NEO_GRB + NEO_KHZ800;
+int pin = 0;              // Ring Led input
+int numPixels = 12;       
+int brightness = 30;      // after that is loaded from eeprom
 
-#define BUTTON_A_PIN  2  // multi mode button pin
+#define BUTTON_A_PIN  2   // multi mode button pin
 Button2 *buttonA;
-int debounce       = 1;  // total debounce value
-int debounce_count = 2;  // init with overreached value
-bool intro;              // animation intro flag
+int debounce       = 1;   // total debounce value
+int debounce_count = 2;   // init with overreached value
+bool intro;               // animation intro flag
 
-int sleeptime  = 10000; // sleep time 10000 ~= 8s 
-int sleepcount = 0;     // sleep counter
-bool onSuspend = true;  // init with reached condition
+#define BODS 7             //BOD Sleep bit in MCUCR
+#define BODSE 2            //BOD Sleep enable bit in MCUCR
+uint8_t mcucr1, mcucr2;    //sleep mcu vars
+int sleeptime  = 10000;   // sleep time 10000 ~= 8s 
+int sleepcount = 0;       // sleep counter
+bool onSuspend = true;    // init with reached condition
+
+#define RNDPIN      2     // for random generator, P4 is analog input 2
 
 uint32_t dice[6][12] = {                   // dice texture numbers
   { 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0},   // dice number 1
@@ -168,7 +167,6 @@ ISR(PCINT0_vect) { }     // This is called when the interrupt occurs, but I don'
 
 void setup() {
   // initialize pseudo-random number generator with some random value
-  pinMode(RNDPIN, INPUT);
   randomSeed(analogRead(RNDPIN));
   // initialize brightness
   uint8_t old_brg=eeprom_read_byte(10);
